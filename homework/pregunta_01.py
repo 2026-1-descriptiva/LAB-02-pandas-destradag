80395a6dacf23e6c86c13b0cb35f7de7e3a85439
destradag <destradag@unal.edu.co>
2026-04-25 13:10:14 -0500

    Submitting

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,13 +4,35 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
+import os
+import pandas as pd
+import glob
 
+def load_specific_inpunt(input_directory,filename):
+    
+    file = glob.glob(f"{input_directory}/*")
+    #file = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    file_path = os.path.join(input_directory, filename)
+    dataframe =pd.read_csv(
+            file_path,
+            delimiter='\t',
+        )
+
+    return dataframe
 
 def pregunta_01():
+
     """
     ¿Cuál es la cantidad de filas en la tabla `tbl0.tsv`?
 
     Rta/
-    40
+    40 """
 
-    """
+    table = load_specific_inpunt("files/input/",'tbl0.tsv')
+    files, cols = table.shape
+    return(files)
+
+
+if __name__ == '__main__':
+    print(pregunta_01())
+
--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -5,6 +5,22 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import glob
+import os
+import pandas as pd
+ 
+def lsi(input_directory,filename):
+    
+    file = glob.glob(f"{input_directory}/*")
+    #file = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    file_path = os.path.join(input_directory, filename)
+    dataframe =pd.read_csv(
+            file_path,
+            delimiter='\t',
+        )
+
+    return dataframe
+
 
 def pregunta_02():
     """
@@ -14,3 +30,9 @@ def pregunta_02():
     4
 
     """
+    df = lsi('files/input','tbl0.tsv')
+    files, cols = df.shape
+    return cols
+
+if __name__ == '__main__':
+    print(pregunta_02())
